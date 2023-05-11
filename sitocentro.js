3d47dcd0b0a7274ba4a3bd486d4295087a33210e
LucaColombo1 <115704164+LucaColombo1@users.noreply.github.com>
2023-05-11 19:14:52 +0000

    molte modifiche generali

---
+++ sitocentro.js
@@ -0,0 +1,23 @@
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.classList.add('white');
+  } else {
+    header.classList.remove('white');
+  }
+});
+
+window.onload = function() {
+  var sectionTitle = document.querySelector('.section-title');
+  var sectionLine = document.querySelector('.section-line');
+
+  var titleWidth = sectionTitle.offsetWidth;
+  var lineWidth = sectionLine.offsetWidth;
+
+  if (titleWidth > lineWidth) {
+    sectionLine.style.width = titleWidth + 'px';
+  } else {
+    sectionTitle.style.width = lineWidth + 'px';
+  }
+}
